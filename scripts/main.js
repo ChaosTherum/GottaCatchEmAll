6149a98c4f94aea1803caea3e91a8afb189fd409
ChaosTherum <chaostherum@gmail.com>
2016-11-28 20:54:19 -0500

    Small stuff
    Fixed some jslint issues.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
+/*jslint plusplus: true */
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
 var caught = 0, progress = 0, pokemon, i, totalPokemon = 722, numberOfBoxes = 32;
-FRAME_TIME = 1000 / 15;
 $.getJSON("json/pokemon.json", "json", function (json) {
+    "use strict";
     pokemon = json;
     totalPokemon = json.length;
 });
 //var client = new $.RestClient("localhost:8000/api/v2/");
 $('#progress').progressbar({max: totalPokemon}, 'enable');
 function populate() {
-    var i, j, k, poke = 1, currentPokemon, boxNumber, m;
+    "use strict";
+    var i, j, k, poke = 1, currentPokemon, boxNumber, m, key, tableHeader, boxCount;
     for (i = 1; i < 33; i++) {
         $("#boxes").append("<table id='box" + i + "' class='box'></table>");
         $('<thead />', {
@@ -21,13 +25,13 @@ function populate() {
                 //text: 'test'
             }).appendTo('#thead' + i + ' > tr');
         }
-        $('<tbody>',{
+        $('<tbody>', {
             id: 'tbody' + i
         }).appendTo('#box' + i);
         for (j = 1; j <= 5; j++) {
             $("#tbody" + i).append("<tr id='row" + j + "'></tr>");
             for (k = 1; k <= 6; k++) {
-              //Calculate the number of the box based on table, column, and row
+                //Calculate the number of the box based on table, column, and row
                 currentPokemon = 30 * (i - 1) + 6 * (j - 1) + (k - 1);
                 boxNumber = currentPokemon + 1;
                 $("#box" + i + " > tbody > #row" + j).append('<td id="tabledata' + k + '" class="box' + boxNumber + '"></td>');
@@ -49,33 +53,36 @@ function populate() {
     }
     // Populate progressbars in boxes
     for (tableHeader = 1; tableHeader <= numberOfBoxes; tableHeader++) {
-      var boxCount = $('#box' + tableHeader + ' input:checkbox').length;
-      console.log(boxCount);
-      console.log(tableHeader)
-      $('#t' + tableHeader + 'h6').progressbar({value: 0, max: boxCount}, 'enable');
+        boxCount = $('#box' + tableHeader + ' input:checkbox').length;
+        $('#t' + tableHeader + 'h6').progressbar({value: 0, max: boxCount}, 'enable');
     }
     $('.ui-progressbar-value').append('<div />');
 }
 
 function update() {
-    progress = $('input:checked').length;
-    percent = (progress / totalPokemon) * 100;
+    "use strict";
+    var progress = $('input:checked').length,
+        percent = (progress / totalPokemon) * 100,
+        tableHeader,
+        boxValue,
+        boxCount;
     $('#progress').progressbar('value', progress);
     $('.ui-progressbar-value > div').text(percent.toPrecision(3) + '% Complete');
     $('#needed').text(totalPokemon - progress + ' Pokemon needed to complete dex');
-    for (tableHeader = 1; tableHeader <= numberOfBoxes; tableHeader++){
-      var boxValue = $('#box' + tableHeader + ' input:checkbox:checked').length
-      var boxCount = $('#box' + tableHeader + ' input:checkbox').length
-      var percent = (boxValue / boxCount) * 100;
-      $('#t' + tableHeader + 'h6').progressbar('value', boxValue);
-      $('#box' + tableHeader + ' .ui-progressbar-value > div').text(percent.toPrecision(3) + '%');
+    for (tableHeader = 1; tableHeader <= numberOfBoxes; tableHeader++) {
+        boxValue = $('#box' + tableHeader + ' input:checkbox:checked').length;
+        boxCount = $('#box' + tableHeader + ' input:checkbox').length;
+        percent = (boxValue / boxCount) * 100;
+        $('#t' + tableHeader + 'h6').progressbar('value', boxValue);
+        $('#box' + tableHeader + ' .ui-progressbar-value > div').text(percent.toPrecision(3) + '%');
     }
 }
 
 $(document).ready(function () {
-  populate();
+    "use strict";
+    populate();
 });
-update();
-$(document).click( function() {
-  update();
+$(document).click(function () {
+    "use strict";
+    update();
 });
